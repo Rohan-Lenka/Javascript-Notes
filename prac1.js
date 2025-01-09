57f788a55966357744900fdd069100f92f630ce2
Rohan Lenka <rohanlenka999@gmail.com>
2025-01-09 23:53:24 +0530

    Update prac1.js

--- prac1.js
+++ prac1.js
@@ -1,14 +1,1 @@
-function setTimeoutPromise(msg, duration) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(msg);
-        }, duration)
-    }).then(msg => console.log(msg));
-}
-
-async function doStuff() {
-    await setTimeoutPromise(`hello`, 2000);
-    await setTimeoutPromise(`world`, 2000);
-    await setTimeoutPromise(`I am Rohan`, 3000);
-}
-doStuff();
+// try your code here 
